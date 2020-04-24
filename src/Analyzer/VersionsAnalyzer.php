8c96dfb7f24df04db8f53142529ea331e8f52db4
TomasVotruba <tomas.vot@gmail.com>
2020-04-24 09:51:38 +0200

    prepare release

---
+++ src/Analyzer/VersionsAnalyzer.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\ChangelogLinker\Analyzer;
+
+use Nette\Utils\Strings;
+use Symplify\ChangelogLinker\Regex\RegexPattern;
+
+final class VersionsAnalyzer
+{
+    /**
+     * @var string[]
+     */
+    private $versions = [];
+
+    public function analyzeContent(string $content): void
+    {
+        $this->versions = [];
+
+        $matches = Strings::matchAll($content, '#\#\# (\[)?' . RegexPattern::VERSION . '(\])?#');
+        foreach ($matches as $match) {
+            $this->versions[] = $match['version'];
+        }
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getVersions(): array
+    {
+        return $this->versions;
+    }
+}
