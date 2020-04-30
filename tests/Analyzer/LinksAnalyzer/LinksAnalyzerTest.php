a129d92ae2319ab223fdd599c6e5db49fb1025f2
TomasVotruba <tomas.vot@gmail.com>
2020-04-30 20:40:10 +0200

    [CS] Deprecate CatchExceptionNameMatchingTypeFixer

---
+++ tests/Analyzer/LinksAnalyzer/LinksAnalyzerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\ChangelogLinker\Tests\Analyzer\LinksAnalyzer;
+
+use Nette\Utils\FileSystem;
+use PHPUnit\Framework\TestCase;
+use Symplify\ChangelogLinker\Analyzer\LinksAnalyzer;
+
+final class LinksAnalyzerTest extends TestCase
+{
+    /**
+     * @var LinksAnalyzer
+     */
+    private $linksAnalyzer;
+
+    protected function setUp(): void
+    {
+        $this->linksAnalyzer = new LinksAnalyzer();
+    }
+
+    public function test(): void
+    {
+        $this->linksAnalyzer->analyzeContent(FileSystem::read(__DIR__ . '/Source/SomeFile.md'));
+
+        $this->assertTrue($this->linksAnalyzer->hasLinkedId('5'));
+        $this->assertFalse($this->linksAnalyzer->hasLinkedId('10'));
+    }
+
+    public function testDeadLinks(): void
+    {
+        $this->linksAnalyzer->analyzeContent(FileSystem::read(__DIR__ . '/Source/SomeFileWithDeadlinks.md'));
+
+        $deadLinks = $this->linksAnalyzer->getDeadLinks();
+
+        $this->assertSame(['5', '15'], $deadLinks);
+    }
+}
