324013d2ab40644dd3b782f46e7d4a8a56d5bf29
TomasVotruba <tomas.vot@gmail.com>
2020-05-12 01:54:55 +0200

    [ECS] drop deprecated psr2 config

---
+++ tests/Analyzer/IdsAnalyzer/IdsAnalyzerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\ChangelogLinker\Tests\Analyzer\IdsAnalyzer;
+
+use Iterator;
+use Nette\Utils\FileSystem;
+use PHPUnit\Framework\TestCase;
+use Symplify\ChangelogLinker\Analyzer\IdsAnalyzer;
+
+final class IdsAnalyzerTest extends TestCase
+{
+    /**
+     * @var IdsAnalyzer
+     */
+    private $idsAnalyzer;
+
+    protected function setUp(): void
+    {
+        $this->idsAnalyzer = new IdsAnalyzer();
+    }
+
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(string $filePath, int $expectedId): void
+    {
+        $content = FileSystem::read($filePath);
+        $this->assertSame($expectedId, $this->idsAnalyzer->getHighestIdInChangelog($content));
+    }
+
+    public function provideData(): Iterator
+    {
+        yield [__DIR__ . '/Source/SomeFile.md', 15];
+        yield [__DIR__ . '/Source/SomeFileWithLinks.md', 20];
+        yield [__DIR__ . '/Source/SomeFileWithAnotherType.md', 428];
+        yield [__DIR__ . '/Source/ShopsysChangelog.md', 449];
+    }
+}
