e26cec7c6fef56269ee6db3079f0eb21a0e4d6d1
TomasVotruba <tomas.vot@gmail.com>
2020-06-25 16:10:20 +0200

    update README

---
+++ src/Analyzer/IdsAnalyzer.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\ChangelogLinker\Analyzer;
+
+use Nette\Utils\Strings;
+
+final class IdsAnalyzer
+{
+    /**
+     * @var string
+     *
+     * Covers cases like:
+     * - #5 Add this => 5
+     * - [#10] Change that => 10
+     */
+    private const PR_REFERENCE_IN_LIST = '#- \[?(\#(?<id>\d+))\]?#';
+
+    public function getHighestIdInChangelog(string $content): ?int
+    {
+        $ids = $this->getAllIdsInChangelog($content);
+        if ($ids === null || $ids === []) {
+            return 0;
+        }
+
+        return (int) max($ids);
+    }
+
+    public function getAllIdsInChangelog(string $content): ?array
+    {
+        $matches = Strings::matchAll($content, self::PR_REFERENCE_IN_LIST);
+        if ($matches === []) {
+            return null;
+        }
+        return array_column($matches, 'id');
+    }
+}
