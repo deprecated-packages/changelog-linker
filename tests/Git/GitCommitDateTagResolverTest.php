cf9e910289545f702f6925709099551e5e9c0d37
TomasVotruba <tomas.vot@gmail.com>
2020-07-05 02:31:24 +0200

    composer: update self.version

---
+++ tests/Git/GitCommitDateTagResolverTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\ChangelogLinker\Tests\Git;
+
+use Iterator;
+use Symplify\ChangelogLinker\Git\GitCommitDateTagResolver;
+use Symplify\ChangelogLinker\HttpKernel\ChangelogLinkerKernel;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+
+final class GitCommitDateTagResolverTest extends AbstractKernelTestCase
+{
+    /**
+     * @var GitCommitDateTagResolver
+     */
+    private $gitCommitDateTagResolver;
+
+    protected function setUp(): void
+    {
+        $this->bootKernel(ChangelogLinkerKernel::class);
+        $this->gitCommitDateTagResolver = self::$container->get(GitCommitDateTagResolver::class);
+
+        $this->markTestSkipped('Random false positives on Github Actions');
+    }
+
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(string $commitHash, string $expectedTag): void
+    {
+        $this->assertSame($expectedTag, $this->gitCommitDateTagResolver->resolveCommitToTag($commitHash));
+    }
+
+    public function provideData(): Iterator
+    {
+        // different commit hashes after split
+        yield ['ef5e708', 'v4.1.1'];
+        yield ['940ec99', 'v3.2.26'];
+        yield ['too-new', 'Unreleased'];
+    }
+
+    /**
+     * @dataProvider provideDataResolveDateForTag()
+     */
+    public function testResolveDateForTag(string $tag, ?string $expectedTag): void
+    {
+        $this->assertSame($expectedTag, $this->gitCommitDateTagResolver->resolveDateForTag($tag));
+    }
+
+    public function provideDataResolveDateForTag(): Iterator
+    {
+        yield ['v4.4.1', '2018-06-07'];
+        yield ['v4.4.2', '2018-06-10'];
+        yield ['Unreleased', null];
+    }
+}
