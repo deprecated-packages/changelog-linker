a581f9524b403aaa996ed89abfc17d7ba82580c0
TomasVotruba <tomas.vot@gmail.com>
2020-07-14 13:21:06 +0200

    [EasyHydrator] yaml config to php

---
+++ src/HttpKernel/ChangelogLinkerKernel.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\ChangelogLinker\HttpKernel;
+
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\HttpKernel\Kernel;
+use Symplify\AutoBindParameter\DependencyInjection\CompilerPass\AutoBindParameterCompilerPass;
+use Symplify\AutowireArrayParameter\DependencyInjection\CompilerPass\AutowireArrayParameterCompilerPass;
+use Symplify\ChangelogLinker\DependencyInjection\CompilerPass\AddRepositoryUrlAndRepositoryNameParametersCompilerPass;
+use Symplify\PackageBuilder\Contract\HttpKernel\ExtraConfigAwareKernelInterface;
+
+final class ChangelogLinkerKernel extends Kernel implements ExtraConfigAwareKernelInterface
+{
+    /**
+     * @var string[]
+     */
+    private $configs = [];
+
+    public function registerContainerConfiguration(LoaderInterface $loader): void
+    {
+        $loader->load(__DIR__ . '/../../config/config.php');
+
+        foreach ($this->configs as $config) {
+            $loader->load($config);
+        }
+    }
+
+    /**
+     * @param string[] $configs
+     */
+    public function setConfigs(array $configs): void
+    {
+        $this->configs = $configs;
+    }
+
+    public function getCacheDir(): string
+    {
+        return sys_get_temp_dir() . '/changelog_linker';
+    }
+
+    public function getLogDir(): string
+    {
+        return sys_get_temp_dir() . '/changelog_linker_log';
+    }
+
+    /**
+     * @return BundleInterface[]
+     */
+    public function registerBundles(): iterable
+    {
+        return [];
+    }
+
+    /**
+     * Order matters!
+     */
+    protected function build(ContainerBuilder $containerBuilder): void
+    {
+        $containerBuilder->addCompilerPass(new AutowireArrayParameterCompilerPass());
+        $containerBuilder->addCompilerPass(new AddRepositoryUrlAndRepositoryNameParametersCompilerPass());
+        $containerBuilder->addCompilerPass(new AutoBindParameterCompilerPass());
+    }
+}
