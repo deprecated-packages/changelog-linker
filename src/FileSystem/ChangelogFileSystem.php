21488e9536d05dc749296825a3f339637b37c0bd
TomasVotruba <tomas.vot@gmail.com>
2020-07-21 22:43:05 +0200

    open 8.2-dev

---
+++ src/FileSystem/ChangelogFileSystem.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\ChangelogLinker\FileSystem;
+
+use Nette\Utils\FileSystem;
+use Symplify\ChangelogLinker\ChangelogLinker;
+use Symplify\ChangelogLinker\Configuration\Option;
+use Symplify\PackageBuilder\Parameter\ParameterProvider;
+use Symplify\SmartFileSystem\FileSystemGuard;
+use Symplify\SmartFileSystem\SmartFileSystem;
+
+final class ChangelogFileSystem
+{
+    /**
+     * @var ChangelogLinker
+     */
+    private $changelogLinker;
+
+    /**
+     * @var ChangelogPlaceholderGuard
+     */
+    private $changelogPlaceholderGuard;
+
+    /**
+     * @var ParameterProvider
+     */
+    private $parameterProvider;
+
+    /**
+     * @var FileSystemGuard
+     */
+    private $fileSystemGuard;
+
+    /**
+     * @var SmartFileSystem
+     */
+    private $smartFileSystem;
+
+    public function __construct(
+        ChangelogLinker $changelogLinker,
+        ChangelogPlaceholderGuard $changelogPlaceholderGuard,
+        FileSystemGuard $fileSystemGuard,
+        ParameterProvider $parameterProvider,
+        SmartFileSystem $smartFileSystem
+    ) {
+        $this->changelogLinker = $changelogLinker;
+        $this->changelogPlaceholderGuard = $changelogPlaceholderGuard;
+        $this->parameterProvider = $parameterProvider;
+        $this->fileSystemGuard = $fileSystemGuard;
+        $this->smartFileSystem = $smartFileSystem;
+    }
+
+    public function readChangelog(): string
+    {
+        $changelogFilePath = $this->getChangelogFilePath();
+        $this->fileSystemGuard->ensureFileExists($changelogFilePath, __METHOD__);
+
+        return $this->smartFileSystem->readFile($changelogFilePath);
+    }
+
+    public function storeChangelog(string $content): void
+    {
+        FileSystem::write($this->getChangelogFilePath(), $content);
+    }
+
+    public function addToChangelogOnPlaceholder(string $newContent, string $placeholder): void
+    {
+        $changelogContent = $this->readChangelog();
+
+        $this->changelogPlaceholderGuard->ensurePlaceholderIsPresent($changelogContent, $placeholder);
+
+        $contentToWrite = sprintf(
+            '%s%s%s<!-- dumped content start -->%s%s<!-- dumped content end -->',
+            $placeholder,
+            PHP_EOL,
+            PHP_EOL,
+            PHP_EOL,
+            $newContent
+        );
+
+        $updatedChangelogContent = str_replace($placeholder, $contentToWrite, $changelogContent);
+
+        $updatedChangelogContent = $this->changelogLinker->processContentWithLinkAppends($updatedChangelogContent);
+
+        $this->storeChangelog($updatedChangelogContent);
+    }
+
+    private function getChangelogFilePath(): string
+    {
+        $fileParameter = $this->parameterProvider->provideParameter(Option::FILE);
+        if (is_string($fileParameter) && file_exists($fileParameter)) {
+            return $fileParameter;
+        }
+
+        return getcwd() . '/CHANGELOG.md';
+    }
+}
