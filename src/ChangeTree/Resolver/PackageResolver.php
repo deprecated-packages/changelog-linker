782a5ac0aac972d3423aafa2223cb33586fda4e7
TomasVotruba <tomas.vot@gmail.com>
2020-08-05 23:43:48 +0200

    remove slevomat cs, breaking build for too many months

---
+++ src/ChangeTree/Resolver/PackageResolver.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\ChangelogLinker\ChangeTree\Resolver;
+
+use Nette\Utils\Strings;
+use Symplify\ChangelogLinker\Configuration\Package;
+
+final class PackageResolver
+{
+    /**
+     * @var string
+     *
+     * It assumes that there is at least one space after the package name.
+     *
+     * It covers:
+     * - "[package-name] "Message => package-name
+     * - "[aliased-package-name] "Message => aliased-package-name
+     * - "[Aliased\PackageName] "Message => Aliased\PackageName
+     * - "[Aliased\PackageName] "Message => Aliased\PackageName
+     */
+    public const PACKAGE_NAME_PATTERN = '#\[(?<package>[-\w\\\\]+)\]( ){1,}#';
+
+    /**
+     * @var string[]
+     */
+    private $packageAliases = [];
+
+    /**
+     * @param string[] $packageAliases
+     */
+    public function __construct(array $packageAliases)
+    {
+        $this->packageAliases = $packageAliases;
+    }
+
+    /**
+     * E.g. "[ChangelogLinker] Add feature XY" => "ChangelogLinker"
+     */
+    public function resolvePackage(string $message): string
+    {
+        $match = Strings::match($message, self::PACKAGE_NAME_PATTERN);
+        if (! isset($match['package'])) {
+            return Package::UNKNOWN;
+        }
+
+        return $this->packageAliases[$match['package']] ?? $match['package'];
+    }
+}
