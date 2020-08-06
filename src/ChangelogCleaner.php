199e00c338392f9f0add6b752f62e71c4528d56a
TomasVotruba <tomas.vot@gmail.com>
2020-08-06 16:10:08 +0200

    [CS] Add constant to configure LineLenghtFixer

---
+++ src/ChangelogCleaner.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\ChangelogLinker;
+
+use Nette\Utils\Strings;
+use Symplify\ChangelogLinker\Analyzer\LinksAnalyzer;
+
+final class ChangelogCleaner
+{
+    /**
+     * @var LinksAnalyzer
+     */
+    private $linksAnalyzer;
+
+    public function __construct(LinksAnalyzer $linksAnalyzer)
+    {
+        $this->linksAnalyzer = $linksAnalyzer;
+    }
+
+    public function processContent(string $changelogContent): string
+    {
+        $this->linksAnalyzer->analyzeContent($changelogContent);
+
+        $deadLinks = $this->linksAnalyzer->getDeadLinks();
+
+        foreach ($deadLinks as $deadLink) {
+            $deadLinkPattern = sprintf('#\[\#?(%s)\]:(.*?)\n#', preg_quote($deadLink));
+            $changelogContent = Strings::replace($changelogContent, $deadLinkPattern);
+        }
+
+        return rtrim($changelogContent) . PHP_EOL;
+    }
+}
