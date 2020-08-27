06af8d952a730b471f8ed01321e70972c99c8045
Tomas Votruba <tomas.vot@gmail.com>
2020-08-27 18:37:25 +0200

    [CodingStandard] Skip parent-enforced reference in NoReferenceRule (#2097)

---
+++ src/Console/Input/PriorityResolver.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\ChangelogLinker\Console\Input;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symplify\ChangelogLinker\Configuration\Option;
+use Symplify\PackageBuilder\Reflection\PrivatesAccessor;
+
+final class PriorityResolver
+{
+    /**
+     * @var PrivatesAccessor
+     */
+    private $privatesAccessor;
+
+    public function __construct()
+    {
+        $this->privatesAccessor = new PrivatesAccessor();
+    }
+
+    /**
+     * Detects the order in which "--in-packages" and "--in-categories" are both called.
+     * The first has a priority.
+     */
+    public function resolveFromInput(InputInterface $input): ?string
+    {
+        $rawOptions = $this->privatesAccessor->getPrivateProperty($input, 'options');
+
+        $requiredOptions = [Option::IN_PACKAGES, Option::IN_CATEGORIES];
+
+        if (count(array_intersect($requiredOptions, array_keys($rawOptions))) !== count($requiredOptions)) {
+            return null;
+        }
+
+        foreach (array_keys($rawOptions) as $name) {
+            if ($name === Option::IN_PACKAGES) {
+                return 'packages';
+            }
+
+            return 'categories';
+        }
+
+        return null;
+    }
+}
---
+++ tests/ChangelogLinker/ChangelogLinkerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\ChangelogLinker\Tests\ChangelogLinker;
+
+use Iterator;
+use Symplify\ChangelogLinker\ChangelogLinker;
+use Symplify\ChangelogLinker\HttpKernel\ChangelogLinkerKernel;
+use Symplify\EasyTesting\DataProvider\StaticFixtureFinder;
+use Symplify\EasyTesting\StaticFixtureSplitter;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+/**
+ * @covers \Symplify\ChangelogLinker\ChangelogLinker
+ * @covers \Symplify\ChangelogLinker\Worker\UserReferencesWorker
+ * @covers \Symplify\ChangelogLinker\Worker\LinkifyWorker
+ * @covers \Symplify\ChangelogLinker\Worker\LinksToReferencesWorker
+ * @covers \Symplify\ChangelogLinker\Worker\DiffLinksToVersionsWorker
+ * @covers \Symplify\ChangelogLinker\Worker\BracketsAroundReferencesWorker
+ */
+final class ChangelogLinkerTest extends AbstractKernelTestCase
+{
+    /**
+     * @var ChangelogLinker
+     */
+    private $changelogLinker;
+
+    protected function setUp(): void
+    {
+        $this->bootKernelWithConfigs(ChangelogLinkerKernel::class, [__DIR__ . '/config/test_config.yaml']);
+        $this->changelogLinker = self::$container->get(ChangelogLinker::class);
+    }
+
+    /**
+     * @dataProvider dataProvider()
+     */
+    public function test(SmartFileInfo $fixtureFileInfo): void
+    {
+        [$inputContent, $expectedContent] = StaticFixtureSplitter::splitFileInfoToInputAndExpected($fixtureFileInfo);
+
+        $processedContent = $this->changelogLinker->processContentWithLinkAppends($inputContent);
+        $this->assertSame($expectedContent, $processedContent, $fixtureFileInfo->getRelativeFilePathFromCwd());
+    }
+
+    public function dataProvider(): Iterator
+    {
+        return StaticFixtureFinder::yieldDirectory(__DIR__ . '/Fixture', '*.md');
+    }
+}
