7493aaa3d3795fae9d1fb961d213c8a074986bfe
Thomas Landauer <thomas@landauer.at>
2020-10-14 13:45:22 +0200

    Update README.md (#2368)
    Co-authored-by: Tomas Votruba <tomas.vot@gmail.com>

---
+++ src/Analyzer/LinksAnalyzer.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\ChangelogLinker\Analyzer;
+
+use Nette\Utils\Strings;
+use Symplify\ChangelogLinker\Regex\RegexPattern;
+
+/**
+ * @see \Symplify\ChangelogLinker\Tests\Analyzer\LinksAnalyzer\LinksAnalyzerTest
+ */
+final class LinksAnalyzer
+{
+    /**
+     * @var string
+     * @see https://regex101.com/r/8L3ZvQ/1
+     */
+    private const REFERENCE_REGEX = '#\[\#?(?<reference>[(-\/@\w\d\.]+)\](?!:)(?!\()#';
+
+    /**
+     * @var string[]
+     */
+    private $linkedIds = [];
+
+    /**
+     * @var string[]
+     */
+    private $references = [];
+
+    public function analyzeContent(string $content): void
+    {
+        // [content]: url
+        $this->linkedIds = [];
+        foreach (Strings::matchAll($content, RegexPattern::LINK_REFERENCE_REGEX) as $match) {
+            $this->linkedIds[] = $match['reference'];
+        }
+        $this->linkedIds = array_unique($this->linkedIds);
+
+        // [content]
+        $this->references = [];
+        foreach (Strings::matchAll($content, self::REFERENCE_REGEX) as $match) {
+            $this->references[] = $match['reference'];
+        }
+        $this->references = array_unique($this->references);
+    }
+
+    public function hasLinkedId(string $id): bool
+    {
+        return in_array($id, $this->linkedIds, true);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getDeadLinks(): array
+    {
+        $deadLinks = array_diff($this->linkedIds, $this->references);
+
+        // special link, that needs to be kept
+        $commentPosition = array_search('comment', $deadLinks, true);
+        if ($commentPosition !== false) {
+            unset($deadLinks[$commentPosition]);
+        }
+
+        return array_values($deadLinks);
+    }
+}
---
+++ tests/ChangelogLinker/Fixture/user_thanks.md
@@ -0,0 +1,12 @@
+# Changelog
+
+- Thanks [@Peter] and [@Tomas]
+
+[@Tomas]: https://github.com/Tomas
+-----
+# Changelog
+
+- Thanks [@Peter] and [@Tomas]
+
+[@Tomas]: https://github.com/Tomas
+[@Peter]: https://github.com/Peter
